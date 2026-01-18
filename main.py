43a29af333180015a0fc4bf0c00233c985e84334
jweaver682 <jweaver682@gmail.com>
2026-01-18 01:47:39 -0500

    Changed gameloop updates to use groups

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def main():
 
     dt = 0
 
+    updatable = pygame.sprite.Group()
+
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while 1:
@@ -30,9 +36,14 @@ def main():
 
         screen.fill("black")
 
-        player.draw(screen)
+        #player.update(dt)
+        #player.draw(screen)
 
-        player.update(dt)
+        updatable.update(dt)
+
+        for drawable_item in drawable:
+
+            drawable_item.draw(screen)
 
         pygame.display.flip()
 
