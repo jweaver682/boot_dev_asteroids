0cfcd84f7e99bba23a092c834bb3b197dbdc2e45
jweaver682 <jweaver682@gmail.com>
2026-01-18 07:11:18 -0500

    Added shoot cooldown timer

--- player.py
+++ player.py
@@ -11,6 +11,8 @@ class Player(CircleShape):
 
         self.rotation = 0
 
+        self.shot_cooldown_timer = 0
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -23,9 +25,13 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
     def rotate(self, dt):
+
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+
+        self.shot_cooldown_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -51,6 +57,12 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+
+        if self.shot_cooldown_timer > 0:
+
+            return
+        
+        self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
 
         x = self.position.x
 
